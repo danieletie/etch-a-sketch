45913b4334b0f8ac0977188fefba3519792dd1c0
danieletie <109879077+danieletie@users.noreply.github.com>
2024-12-03 15:30:33 +0100

    Add more functionality to etch-a-sketch
    - function to create grid based on input
    - function to create grid
    - function to remove clear existing grid on click of button
    - handle error when empty grid receives no input
    - make cell size flexible and a function of number of squares
      and fixed grid size

--- javascript.js
+++ javascript.js
@@ -1,19 +1,67 @@
 let grid = document.querySelector('.grid');
+let gridSize = 0;
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
+let numOfSquares = document.querySelector('.grid-size');
 
-    
+
+
+numOfSquares.addEventListener('click', () => {
+
+  //run remove grid function to remove existing grid
+  if (gridSize !== null) {
+    removeGrid();
+  };
+
+  //prompt to input size of new grid
+  gridSize = prompt('Please enter a number: ');
+
+  //check to make sure input is a number between 1 and 100
+  if (isNaN(gridSize)) {
+    alert('That\'s not a valid number. Please try again'); 
+  } else if ((Number(gridSize)) <= 0 || (Number(gridSize)) > 100){
+    alert(`Please enter avalue between 1 and 100`);
+  } else {
+    gridSize = Number(gridSize);
+    console.log(gridSize);
+    //run function to create grid based on number of squares inputed
+    createGrid();
+    createSketch();
+  };
+});
+
+
+function createGrid() {
+  let cellSize = 500 / gridSize;
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      grid.appendChild(cell);
+      cell.style.height = cellSize + "px";
+      cell.style.width = cellSize + "px";
+    };
   };
 };
 
-let cellTarget = document.querySelectorAll('.cell');
 
-cellTarget.forEach((cell) => {
-  cell.addEventListener('mouseenter', (e) => {
-    cell.style.backgroundColor = 'blue';
+function removeGrid () {
+  for (let i = gridSize; i > 0; i--) {
+    for (let j = gridSize; j > 0; j--) {
+      let emptyCell = document.querySelector('.cell');
+      emptyCell.remove();
+    };
+  };
+};
+
+
+function createSketch () {
+
+  let cellTarget = document.querySelectorAll('.cell');
+
+  cellTarget.forEach((cell) => {
+    cell.addEventListener('mouseenter', (e) => {
+      cell.style.backgroundColor = 'blue';
+    });
   });
-});
+};
+
